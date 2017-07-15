f0f2b5cbfe649a1412aedd3573aea14d861758ce
Matheus Yan <matt_yn@outlook.com>
2017-07-15 16:31:52 -0300

    Alterei tal coisa

--- LJMSOFT/View/PessoaView/TelaRegistro.Designer.cs
+++ LJMSOFT/View/PessoaView/TelaRegistro.Designer.cs
@@ -780,7 +780,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.BackColor = System.Drawing.Color.Beige;
             this.ClientSize = new System.Drawing.Size(1010, 495);
             this.Controls.Add(this.emailBox);
             this.Controls.Add(this.celularBox);
